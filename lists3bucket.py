9f08f96baf6d37ab5ffe9b32eda88e863dc98aa5
Michael Perry <mperry777@gmail.com>
2017-11-08 14:04:34 -0500

    Added argument handling for a valid s3 bucketname

--- lists3bucket.py
+++ lists3bucket.py
@@ -6,19 +6,26 @@ List contents of an AWS S3 bucket
 Replace <bucketname> with a valid s3 bucket name
 """
 
+import argparse
 import boto3
 
-c = boto3.client('s3')
-
 def main():
-    try:
-        r = c.list_objects(Bucket='<bucketname>')
+    """
+    get s3 bucket contents and print
+    """
 
-        for i in range(len(r['Contents'])):
-            print(r['Contents'][i]['Key'])
-    except Exception as e:
-        print(e)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bucketname", help="A valid AWS S3 bucket name")
+    args = parser.parse_args()
+
+    try:
+        client = boto3.client('s3')
+        result = client.list_objects(Bucket=args.bucketname)
+
+        for i in range(len(result['Contents'])):
+            print(result['Contents'][i]['Key'])
+    except Exception as exception:
+        print(exception)
 
 if __name__ == '__main__':
     main()
-
