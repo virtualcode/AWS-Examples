d20b29e4112bd18f9ca95617c081e7acd5fa01e3
Michael Perry <mperry777@gmail.com>
2017-11-08 13:14:40 -0500

    Added exception handling and documentation comments

--- lists3bucket.py
+++ lists3bucket.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
-# List contents of an AWS S3 bucket
+
+
+"""
+List contents of an AWS S3 bucket
+Replace <bucketname> with a valid s3 bucket name
+"""
 
 import boto3
 
 c = boto3.client('s3')
-r = c.list_objects(Bucket='<bucketName>')
 
-for i in range(len(r['Contents'])):
-    print(r['Contents'][i]['Key'])
+def main():
+    try:
+        r = c.list_objects(Bucket='<bucketname>')
+
+        for i in range(len(r['Contents'])):
+            print(r['Contents'][i]['Key'])
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
 
