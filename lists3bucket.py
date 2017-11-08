99d1a87ad189fa5ffe75e7762f33dd2836223371
Michael Perry <mperry777@gmail.com>
2017-11-08 09:34:02 -0500

    Initial commit

---
+++ lists3bucket.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# List contents of an AWS S3 bucket
+
+import boto3
+
+c = boto3.client('s3')
+r = c.list_objects(Bucket='bucketName')
+
+for i in range(len(r['Contents'])):
+    print(r['Contents'][i]['Key'])
+
